5dae576520364d58d0976223649bbf2e4b556882
Rick Matta <rickmatta@gmail.com>
2020-09-14 11:11:53 -0400

    sample ads working on ios but not android

--- lib/ad_manager.dart
+++ lib/ad_manager.dart
@@ -3,9 +3,9 @@ import 'dart:io';
 class AdManager {
   static String get appId {
     if (Platform.isAndroid) {
-      return "ca-app-pub-2650722762036011~5561117207";
+      return "ca-app-pub-3940256099942544~4354546703";
     } else if (Platform.isIOS) {
-      return "ca-app-pub-2650722762036011~3385745541";
+      return "ca-app-pub-3940256099942544~2594085930";
     } else {
       throw new UnsupportedError("Unsupported platform");
     }
@@ -13,9 +13,9 @@ class AdManager {
 
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return "ca-app-pub-2650722762036011/9583945085";
+      return "ca-app-pub-3940256099942544/8865242552";
     } else if (Platform.isIOS) {
-      return "ca-app-pub-2650722762036011/4369779873";
+      return "ca-app-pub-3940256099942544/4339318960";
     } else {
       throw new UnsupportedError("Unsupported platform");
     }
