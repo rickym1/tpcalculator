d0b4925336282521bd8f6d481ca55f55a49b119c
Rick Matta <rickmatta@gmail.com>
2020-12-19 15:17:46 -0500

    gesture detectorfor keypad completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,30 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 import 'HomePage.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Calculator App",
-      theme: ThemeData(
-        primarySwatch: Colors.teal
+    return GestureDetector(
+      onTap: () {
+        if (Platform.isIOS) hideKeyboard(context);
+      },
+      child: MaterialApp(
+        title: "Calculator App",
+        theme: ThemeData(primarySwatch: Colors.teal),
+        home: HomePage(),
       ),
-      home: HomePage(),
     );
+  }
+
+  void hideKeyboard(BuildContext context) {
+    FocusScopeNode currentFocus = FocusScope.of(context);
+    if (!currentFocus.hasPrimaryFocus && currentFocus.focusedChild != null) {
+      FocusManager.instance.primaryFocus.unfocus();
+    }
   }
 }
